bd4838cae6b8a1572299e96ca8fab118ce337e2d
+ <+>
2022-07-02 10:29:12 -0600

    commit tree 09a4681fc64b6877bd522e9313647649502779f2

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    lengthFilterArray = array.filter { |item| item.length == word.length }
+    if lengthFilterArray == []
+      lengthFilterArray
+    else
+      matchedWord = lengthFilterArray.filter do |item|
+        itemArray = item.chars
+        sortedLetterArray = itemArray.sort
+      
+        wordArray = word.chars
+        sortedWord = wordArray.sort
+
+        sortedLetterArray == sortedWord
+      end
+    end
+    matchedWord
+  end
+end
+
+
+sample = Anagram.new("the")
+sample.match(["hte", "eth", "tttt"])
